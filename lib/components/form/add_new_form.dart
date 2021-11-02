9e429ecc8b2ef0410885c5e6d899e373fa9958cd
Efekan Gündüz <2015010206048@ogrenci.karabuk.edu.tr>
2021-11-02 23:20:13 +0300

    Profile page and profile photo

---
+++ lib/components/form/add_new_form.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/styles/custom_theme.dart';
+
+class AddNewForm extends StatefulWidget {
+  _AddNewFormState createState() => _AddNewFormState();
+}
+
+class _AddNewFormState extends State<AddNewForm> {
+  String newTitle = '';
+  String description = '';
+  String source = '';
+
+  final _formkey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(20),
+          topLeft: Radius.circular(20),
+        ),
+      ),
+      child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              TextFormField(
+                key: ValueKey('newTitle'),
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.title,
+                    color: Color(0xffDCA11E),
+                  ),
+                  labelText: 'New Title',
+                ),
+                validator: (value) {
+                  if (value.toString().length < 6) {
+                    return 'New title is too short';
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (value) {
+                  setState(() {
+                    newTitle = value!;
+                  });
+                },
+              ),
+              TextFormField(
+                key: ValueKey('description'),
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.description,
+                    color: Color(0xffDCA11E),
+                  ),
+                  labelText: 'Description',
+                ),
+                validator: (value) {
+                  if (value.toString().length < 15) {
+                    return 'Description is too short';
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (value) {
+                  setState(() {
+                    description = value!;
+                  });
+                },
+              ),
+              TextFormField(
+                key: ValueKey('source'),
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.source,
+                    color: Color(0xffDCA11E),
+                  ),
+                  labelText: 'Source',
+                ),
+                obscureText: true,
+                validator: (value) {
+                  if (value.toString().length < 6) {
+                    return 'Source is too short';
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (value) {
+                  setState(() {
+                    source = value!;
+                  });
+                },
+              ),
+              TextFormField(
+                key: ValueKey('details'),
+                keyboardType: TextInputType.multiline,
+                minLines: 2,
+                maxLines: 8,
+                maxLength: 1000,
+                decoration: InputDecoration(
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(0.0),
+                      child:
+                          Icon(Icons.comment, size: 40.0, color: primaryColor),
+                    ),
+                    hintText: "Details",
+                    hintStyle: TextStyle(color: blackColor),
+                    border: OutlineInputBorder(),
+                    labelStyle: TextStyle(color: blackColor)),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: blackColor,
+                  fontSize: 25.0,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Empty value";
+                  }
+                },
+              ),
+              TextButton(
+                child: Text('Add Photo'),
+                onPressed: () {},
+              ),
+              TextButton(
+                onPressed: () {
+                  if (_formkey.currentState!.validate()) {
+                    _formkey.currentState!.save();
+                  }
+                },
+                style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    backgroundColor: primaryColor),
+                child: Container(
+                  width: screenSize.width * 0.3,
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Add New',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
